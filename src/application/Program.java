542a5e50dabde6ae43dd64d1fe5f6da3178f71cc
DiegoVP66 <dvpdev33@gmail.com>
2021-05-29 13:44:34 -0300

    Comentários adicionados

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,17 +10,22 @@ public class Program {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		List<Integer> list = new ArrayList<>();
-
+		System.out.print("Informe a quantidade de valores: ");
 		int n = sc.nextInt();
 		int[] linhas = new int[n];
 		for (int i = 0; i < linhas.length; i++) {
+			
+			System.out.printf("%d° valor: ", i+1);
 			linhas[i] = sc.nextInt();
 		
 			list.add(linhas[i]);
 		}
-
+        
+		// Ordena em ordem crescente e filtra os números pares
 		Collections.sort(list);
 		list.stream().filter(x -> x % 2 == 0).map(x -> x).forEach(System.out::println);
+		
+		// Ordena em ordem decrescente e filtra os números pares
 		Collections.reverse(list);
 		list.stream().filter(x -> x % 2 != 0).map(x -> x).forEach(System.out::println);
 
